5e3de731c1695f7d38461cee66e3c71e7ca8d08c
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-28 22:17:20 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -35,7 +35,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
-  holiday_supplies[:summer][:fourth_of_july][1]
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 second_supply_for_fourth_of_july(holiday_supplies)
