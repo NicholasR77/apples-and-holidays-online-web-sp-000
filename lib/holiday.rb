df202515ee562dff6d5c63fc9c329db24230b03d
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-28 22:23:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
   holiday_hash = {
     season => {
       holiday_name => {
-        supply_array
       }
     }
   }
