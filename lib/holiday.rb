816a79dbbdb55bee14ccd39c259b090c1ca92457
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-28 22:06:53 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -42,7 +42,7 @@ def second_supply_for_fourth_of_july(holiday_hash)
   end
 end
 
-puts second_supply_for_fourth_of_july(holiday_supplies)
+second_supply_for_fourth_of_july(holiday_supplies)
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
