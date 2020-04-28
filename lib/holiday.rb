84ef089f0a60b933bbd77337a17ad5dd3d213307
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-28 22:34:00 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,6 +75,8 @@ def all_winter_holiday_supplies(holiday_hash)
   puts all_supplies
 end
 
+all_winter_holiday_supplies(test)
+
 def all_supplies_in_holidays(holiday_hash)
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:
