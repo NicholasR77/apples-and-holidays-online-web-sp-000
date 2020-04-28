9e48711dede7f840cbde2d34c8c1531caf396ce8
Nicholas Robbins <nl7robbins@icloud.com>
2020-04-28 22:45:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -103,11 +103,11 @@ all_supplies_in_holidays(test)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_supplies = []
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supply|
       if (supply.include?("BBQ") == true)
-        puts holiday
-        return holiday
+        bbq_supplies.push(holiday)
       end
     end
   end
